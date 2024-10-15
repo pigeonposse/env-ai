e0693037980c4d34decdff9441ba70c0f06a59ab
angelespejo <angelespejo13@gmail.com>
2024-10-15 19:35:46 +0200

    :memo: docs(core): change user texts

--- src/cli/core/model.ts
+++ src/cli/core/model.ts
@@ -23,7 +23,7 @@ export class CoreModel extends CoreSuper {
 
 				const ollamaInfo = `\n\n   For more information about Ollama please visit ${this._c.gray( 'https://ollama.com/' )}` 
 				this._errorRes( 'Error retrieving model from Ollama.', '' )
-				if ( e.message === this._ai.error.NO_MODELS ) throw new this.Error( 'No Ollama models found.\n   Please download at least one model.' + ollamaInfo )
+				if ( e instanceof Error && e.message === this._ai.error.NO_MODELS ) throw new this.Error( 'No Ollama models found.\n   Please download at least one model.' + ollamaInfo )
 				else throw new this.Error( 'Ollama is not installed or is not running.\n   Please install/start Ollama to use this tool.' + ollamaInfo )
 			
 			}
@@ -40,7 +40,7 @@ export class CoreModel extends CoreSuper {
 		else if ( modelArgv && !models.includes( modelArgv ) ) log.warn( `Model [${modelArgv}] not exists or is not installed.` )
 		
 		const model = await select( {
-			message : 'Select an Ollama model to use for suggestions:',
+			message : 'Select a model to use:',
 			options : models.map( modelName => ( {
 				value : modelName,
 				label : modelName, 
