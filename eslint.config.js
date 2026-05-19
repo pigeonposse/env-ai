b6b8ac79ca36ffab512f5893c130161df966a525
angelespejo <angelespejo13@gmail.com>
2026-05-20 01:14:09 +0200

    :green_heart: ci(env): up env

--- eslint.config.js
+++ eslint.config.js
@@ -7,21 +7,27 @@
  * @see https://dovenv.pigeonposse.com/guide/plugin/lint
  */
 
-// import { setConfig } from '@dovenv/theme-pigeonposse/eslint'
+import { setConfig }    from '@dovenv/theme-pigeonposse/eslint'
+import { defineConfig } from 'eslint/config'
 
-// const config = setConfig( {
-// 	general   : 'ts',
-// 	jsdoc     : true,
-// 	gitignore : true,
-// 	package   : true,
-// 	json      : true,
-// 	toml      : true,
-// 	md        : true,
-// 	ignore    : [ 
-// 		'**/README.md',
-// 		'**/CHANGELOG.md',
-// 		'**/docs/**'
-// 	 ],
-// } )
-// // console.log(config)
-// export default config
+const config = setConfig( {
+	general   : 'ts',
+	jsdoc     : true,
+	gitignore : true,
+	package   : true,
+	json      : true,
+	toml      : true,
+	md        : true,
+	ignore    : [
+		'**/README.md',
+		'**/CHANGELOG.md',
+		'**/docs/**',
+	],
+} )
+// console.log(config)
+export default defineConfig( config, {
+	rules : {
+		'package-json/no-redundant-publishConfig' : 'off',
+		'package-json/valid-engines'              : 'off',
+	},
+} )
